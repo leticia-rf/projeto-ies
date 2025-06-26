fc6ea676ae824b23cff1534449eac0185242d108
gabriel <gabrielmatheusdesouza26@gmail.com>
2025-06-26 08:54:26 -0300

    Nova main

--- src/Main.java
+++ src/Main.java
@@ -7,30 +7,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        adicionarProduto("1Leite Condensado Piracanjuba 395g", 6.47, "src/imagens/piracanjuba.png");
-        adicionarProduto("2Leite Condensado Piracanjuba 395g", 6.47, "src/imagens/piracanjuba.png");
-        adicionarProduto("3Leite Condensado Piracanjuba 395g", 6.47, "src/imagens/piracanjuba.png");
-        adicionarProduto("4Leite Condensado Piracanjuba 395g", 6.47, "src/imagens/piracanjuba.png");
-        adicionarProduto("5Leite Condensado Piracanjuba 395g", 6.47, "src/imagens/piracanjuba.png");
-        adicionarProduto("6Leite Condensado Piracanjuba 395g", 6.47, "src/imagens/piracanjuba.png");
-        adicionarProduto("7Leite Condensado Piracanjuba 395g", 6.47, "src/imagens/piracanjuba.png");
+        Produto p1 = new Produto("Leite Condensado Piracanjuba", 6.47, "src/imagens/piracanjuba.png");
+        Produto p2 = new Produto("Leite Condensado Frimesa", 6.79, "src/imagens/frimesa.png");
+        Produto p3 = new Produto("Leite Condensado Italac", 6.65, "src/imagens/italac.png");
+        Produto p4 = new Produto("Leite Condensado Camponesa", 8.49, "src/imagens/camponesa.png");
+        Produto p5 = new Produto("Leite Condensado Moça", 8.98, "src/imagens/nestle.png");
+        Produto p6 = new Produto("Leite Condensado São Lourenço", 36.00, "src/imagens/saolourenco.png");
+        Produto p7 = new Produto("Leite Condensado Itambé", 7.89, "src/imagens/itambe.png");
 
-        Produto p = new Produto("teste", 1.0, "src/imagens/itambe.png");
-        p.salvarInformacoes("pagina2");
 
-        //mostrarImagem(1);
+        p1.adicionarAvaliacao("José da Silva", "Muito doce, adorei", 5);
+        p1.adicionarAvaliacao("Mário", "Muito doce, odiei", 1);
+        p1.adicionarAvaliacao("Luigi", "Meu pudim ficou torto, mas estava bom", 4);
+
+        p2.adicionarAvaliacao("José da Silva", "Piracanjuba é melhor...", 3);
+
+        p3.adicionarAvaliacao("Otávio Inácio", "Muito enjoativo, não gostei", 1);
+
+        p4.ativarPromocao(4.49);
+        p4.adicionarAvaliacao("Ash Ketchum", "Comprei na promoção e não me arrependo", 5);
+
+        p6.adicionarAvaliacao("Anônimo", "Pelo preço deveria vim o pudim feito", 3);
+
+        p7.adicionarAvaliacao("Davi Leal", "Leite incomparável!", 5);
+
+        adicionarProduto(p1);
+        adicionarProduto(p2);
+        adicionarProduto(p3);
+        adicionarProduto(p4);
+        adicionarProduto(p5);
+        adicionarProduto(p6);
+        adicionarProduto(p7);
 
         salvarPaginas();
-
-
     }
 
-    public static void adicionarProduto(String d, double p, String i){
+    public static void adicionarProduto(Produto p){
         if(numProdutos >= numPaginas * Pagina.getMaximoProdutos() && !criarPagina())
             System.out.println("Máximo de produtos atingido!");
         else
             try {
-                paginas[numPaginas - 1].adicionarProduto(new Produto(d, p, i));
+                paginas[numPaginas - 1].adicionarProduto(p);
                 numProdutos++;
             }catch (IllegalArgumentException ae){
                 System.out.println("Produto inválido: " + ae.getMessage());
